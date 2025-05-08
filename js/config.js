0ae3f81855b52a835e5da4873840c6e82450c554
JaredInTech <jaredrpeterson@gmail.com>
2025-05-07 22:03:52 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-2_Y2A7zKkvJ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '40d82jt7u1k2njfult2g87urfe', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '56egrkjf9djgsarq75m13dvv8r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
